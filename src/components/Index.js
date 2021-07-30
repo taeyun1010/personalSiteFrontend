0d843e76c4e87a47e579dde9ea68438c58387710
Taeyun Kim <taeyun1010@yahoo.ca>
2021-07-31 05:16:46 +0900

    공지사항과 게시판을 메인 페이지에 추가함

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import NoticeBoard from "./NoticeBoard";
+import Board from "./Board";
 
 const Index = () => {
   return (
-    <div style={{ marginTop: "100px" }}>
-      this website is still under development
-      <img src="img/background.jpg" width="100%" height="auto"></img>
+    <div style={{ marginBottom: "200px" }}>
+      <div style={{ marginTop: "100px", marginBottom: "20px" }}>
+        this website is still under development
+        <img src="img/background.jpg" width="30%" height="auto"></img>
+      </div>
+      <h1>공지사항</h1>
+      <NoticeBoard></NoticeBoard>
+      <h1 style={{ marginTop: "20px" }}>게시판</h1>
+      <Board></Board>
     </div>
   );
 };
